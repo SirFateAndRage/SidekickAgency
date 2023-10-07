bbfc19bc9d0fad25e699ef514601a358625bbeff
SirFateAndRage <milanicristian94@gmail.com>
2023-10-07 04:19:34 -0300

    [A] imageTest

---
+++ Assets/Script/Efficiency/FloatingImage.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Menace
+{
+    public class FloatingImage : MonoBehaviour
+    {
+        [SerializeField] private Transform _mainCamera;
+        [SerializeField] private Transform _building;
+
+        [SerializeField] private Vector3 _offSett;
+
+        private void Start()
+        {
+            transform.position = _building.position + _offSett;
+
+            transform.LookAt(_mainCamera);
+        }
+    }
+}
