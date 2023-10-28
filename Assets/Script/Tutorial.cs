ee8a225df50c9505ea00cdc21574b8ab6646c6bd
Garcia Maximiliano <garciamaximiliano.1996@gmail.com>
2023-10-28 04:03:13 -0300

    Revert "newTutorialWindow"
    This reverts commit 56bada52ef2fe09367260fdaa5c3e9fff5f6d175.

--- Assets/Script/Tutorial.cs
+++ Assets/Script/Tutorial.cs
@@ -6,24 +6,11 @@ public class Tutorial : MonoBehaviour
 {
     [SerializeField] GameObject _gameMotor;
     [SerializeField] GameObject _tutorialMessage;
-    [SerializeField] GameObject _flavorText;
-
-    int clicks = 0;
 
     public void BeginGame()
     {
-        if (clicks == 1)
-        {
-            _gameMotor.SetActive(true);
-            _flavorText.SetActive(false);
-            Destroy(this.gameObject);
-
-        }
-        else
-        {
-            _tutorialMessage.SetActive(false);
-            _flavorText.SetActive(true);
-        }
-        clicks++;
+        _gameMotor.SetActive(true);
+        _tutorialMessage.SetActive (false);
+        Destroy(this.gameObject);
     }
 }
